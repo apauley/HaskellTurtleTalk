43b92c121f46de4d67369e5bfa268e34253f5f06
Andreas Pauley <andreas@pauley.org.za>
2016-07-10 19:51:26 +0200

    Add a command-line parser for generate

--- generate.hs
+++ generate.hs
@@ -5,5 +5,14 @@
 
 import Turtle
 
+-- Run as a script using `./genarate.hs`
+-- or compile using:
+-- stack ghc -- -O2 generate.hs
 main = do
-  proc "pandoc" ["-t", "slidy", "-s", "slides.md", "-o", "slides.html"] empty
+  selfContained <- options "Generate the slides as html" parser
+  let baseArgs = ["-t", "slidy", "-s", "slides.md", "-o", "slides.html"]
+  let args = if (selfContained) then "--self-contained":baseArgs else baseArgs
+  proc "pandoc" args empty
+
+parser :: Parser Bool
+parser = switch "self-contained" 's' "Include JS/CSS within the HTML file"
